247e084c4f0e6e29d8216e5f17f9e591464a4cca
Roxana Gabor <Roxana Gabor>
2016-12-12 19:50:55 +0200

    Cursul 9, ex: 1,2,3

--- Cursul8/Models/ProjectEvaluation.cs
+++ Cursul8/Models/ProjectEvaluation.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Cursul8.Models
 {
+    [Bind(Exclude = "Country")]
+
+    
     public class ProjectEvaluation
     {
        public string name { get; set; }
+        [StringLength(10)]
        public string city { get; set; }
         public string country { get; set; }
+        [Range(1, 10)]
         public double rating { get; set; }
         public int id { get; set; }
     }
